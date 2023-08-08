42adfa0751957e2ea08dda10ea0965c5953f84dd
Huseynov Yunis <yunisah@code.local>
2023-08-08 15:21:50 +0400

    Navigation Bar and side Bar for tablet and mobile DONE

--- nft_marketplace/javascript/header.js
+++ nft_marketplace/javascript/header.js
@@ -1,5 +1,12 @@
 const burger = document.querySelector(".burger");
+const sideBar = document.querySelector(".sideBar");
+const rightNav = document.querySelector(".right");
+sideBar.innerHTML = rightNav.innerHTML;
 
-burger.addEventListener("click",()=>{
-    
-})
+burger.addEventListener("click", () => {
+  if (sideBar.style.bottom != "-167.17px") {
+    sideBar.style.bottom = "-167.17px";
+  } else {
+    sideBar.style.bottom = "0px";
+  }
+});
