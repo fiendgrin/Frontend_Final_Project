1d021bbcc1f5258fc32641897232d392179a1626
fiendgrin <yunisah@code.edu.az>
2023-08-10 09:28:04 +0400

    Top Rated Artists section responsive done

--- nft_marketplace/javascript/header.js
+++ nft_marketplace/javascript/header.js
@@ -8,11 +8,23 @@ burger.addEventListener("click", () => {
     sideBar.style.bottom = "-167.17px";
     sideBar.style.zIndex = "0";
     sideBar.style.opacity = "1";
-
   } else {
     sideBar.style.bottom = "-100px";
     sideBar.style.zIndex = "-1";
     sideBar.style.opacity = "0";
-
   }
+});
+
+sideBar.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+burger.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+  sideBar.style.bottom = "-100px";
+  sideBar.style.zIndex = "-1";
+  sideBar.style.opacity = "0";
 });
