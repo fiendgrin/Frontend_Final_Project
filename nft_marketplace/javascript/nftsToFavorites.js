957b4c12056c6208bae2f6815f85c231ab25a5cf
fiendgrin <yunisah@code.edu.az>
2023-08-13 23:41:30 +0400

    Add to favourites logic done

--- nft_marketplace/javascript/nftsToFavorites.js
+++ nft_marketplace/javascript/nftsToFavorites.js
@@ -12,8 +12,32 @@ const allNftData = [
   { id: 9, name: "IceCream Ape", image: "9.png" },
 ];
 
-function nftMaker(id, name, image) {
-  return `<div id="${id}" class="nft">
+function nftMaker(id, name, image, favorite) {
+  if (favorite) {
+    return `<div id="${id}" class="nft">
+    <img src="./${image}" alt="" />
+    <div class="bottom">
+      <div class="info">
+        <h5>${name}</h5>
+        <div class="account">
+          <img src="./images/AvatarPlaceholder.png" alt="" />
+          <p>MoonDancer</p>
+        </div>
+      </div>
+      <div class="money">
+        <div class="left">
+          <p>Price</p>
+          <p>1.63 ETH</p>
+        </div>
+        <div class="right">
+          <p>Highest Bid</p>
+          <p>0.33 wETH</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  } else {
+    return `<div id="${id}" class="nft">
     <img src="./images/nftToFavorite/${image}" alt="" />
     <div class="bottom">
       <div class="info">
@@ -37,12 +61,74 @@ function nftMaker(id, name, image) {
     <img id="${id}" class="heart" src="./icons/heart.svg" alt="" />
     <img id="${id}" class="heartHollow" src="./icons/heartHollow.svg" alt="" />
   </div>`;
+  }
 }
 
-allNftData.forEach((nft) => {
-  nfts.innerHTML += nftMaker(nft.id, nft.name, nft.image);
+function nftAdder(data, favorite) {
+  nfts.innerHTML = "";
+  data.forEach((nft) => {
+    nfts.innerHTML += nftMaker(nft.id, nft.name, nft.image, favorite);
+  });
+}
+
+nftAdder(allNftData, false);
+
+let cards = document.querySelectorAll(".nfts .nft");
+let hearts = document.querySelectorAll(".nfts .nft .heart");
+const tabs = document.querySelectorAll("#nsftsToFavorites .top .box .tab");
+
+const allFavouritesData = [];
+
+cardsFunc();
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    if (tab.classList.contains("active")) {
+      return;
+    }
+    tabs.forEach((tab2) => {
+      tab2.classList.remove("active");
+    });
+    tab.classList.add("active");
+    if (tab.textContent.includes("NFTs")) {
+      nftAdder(allNftData, false);
+      cards = document.querySelectorAll(".nfts .nft");
+      hearts = document.querySelectorAll(".nfts .nft .heart");
+      allFavouritesData.forEach((fav) => {
+        hearts.forEach((heart) => {
+          console.log(fav);
+          console.log(heart);
+          if (fav.id == heart.id) {
+            heart.style.opacity = "1";
+          }
+        });
+      });
+    } else {
+      nftAdder(allFavouritesData, true);
+    }
+    cardsFunc();
+  });
 });
 
-const cards = document.querySelectorAll(".nfts .nft");
-
-let allFavouritesData = [];
+function cardsFunc() {
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      hearts.forEach((heart) => {
+        if (card.id == heart.id && heart.style.opacity != "1") {
+          heart.style.opacity = "1";
+          let name = card.children[1].children[0].children[0].innerHTML;
+          let image = card.children[0].getAttribute("src");
+          allFavouritesData.push({ id: card.id, name: name, image: image });
+        } else if (card.id == heart.id && heart.style.opacity == "1") {
+          heart.style.opacity = "0.3";
+          allFavouritesData.forEach((fav, idx) => {
+            if (fav.id == heart.id) {
+              allFavouritesData.splice(idx, 1);
+            }
+          });
+        }
+      });
+      tabs[1].firstElementChild.innerHTML = allFavouritesData.length;
+    });
+  });
+}
